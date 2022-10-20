d24e1459c2fa0110853c1eb8a2725a3985561b15
betocostadev <robertomarx@gmail.com>
2022-10-20 11:19:25 -0300

    added map to web

--- mapFaker/src/index.ts
+++ mapFaker/src/index.ts
@@ -6,7 +6,10 @@ import { Company } from './Company'
 const user = new User()
 const company = new Company()
 
-google.maps.VehicleType.BUS
+const mapDiv = document.getElementById('map')
+console.log(mapDiv)
+// const mapOptions = { zoom:}
+new google.maps.Map(mapDiv, { zoom: 1, center: { lat: company.location.lat, lng: company.location.lng}})
 
 console.log(user)
 console.log(company)
