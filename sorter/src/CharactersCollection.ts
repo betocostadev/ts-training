92b28f7e0963b3c8a7776b994ab2f8ab4c0e0f96
betocostadev <robertomarx@gmail.com>
2022-11-15 16:44:44 -0300

    implemented characters collection to use with sort

--- sorter/src/CharactersCollection.ts
+++ sorter/src/CharactersCollection.ts
@@ -6,15 +6,16 @@ export class CharactersCollection {
 
   // Compare two elements and decide if they need to swap
   compare(leftIndex: number, rightIndex: number): boolean {
-    return this.data[leftIndex] > this.data[rightIndex]
+    return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase()
   }
 
-  // swap(leftIndex: number, rightIndex: number): void {
-  //   const leftHand = this.data[leftIndex]
-  //   this.data[leftIndex] = this.data[rightIndex]
-  //   this.data[rightIndex] = leftHand
-  // }
+  swap(leftIndex: number, rightIndex: number): void {
+    const charArray = this.data.split('')
+    const leftHand = charArray[leftIndex]
+    charArray[leftIndex] = charArray[rightIndex]
+    charArray[rightIndex] = leftHand
 
-
+    this.data = charArray.join('')
+  }
 }
 
--- sorter/src/index.ts
+++ sorter/src/index.ts
@@ -1,5 +1,6 @@
 import { Sorter } from './Sorter'
 import { NumbersCollection } from './NumbersCollection'
+import { CharactersCollection } from './CharactersCollection'
 console.log('Sorter running - build done')
 
 // Check the old_index.ts to understand why we changed the approach
@@ -8,3 +9,8 @@ const numbersColOne = new NumbersCollection([150, -5, 3, 2, 10, -1, 50, 90])
 const sorterOne = new Sorter(numbersColOne)
 sorterOne.sort()
 console.log(numbersColOne.data)
+
+const charsColOne = new CharactersCollection('node')
+const charsSorterOne = new Sorter(charsColOne)
+charsSorterOne.sort()
+console.log(charsColOne.data)
