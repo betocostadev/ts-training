19f96628f2ccaf0f46514cf0fba5a2c8288ba7e7
betocostadev <robertomarx@gmail.com>
2022-11-15 17:47:31 -0300

    added a linkedList class to use with the sorter

---
+++ sorter/src/LinkedList.ts
@@ -0,0 +1,98 @@
+// Linked list
+// | ----- Node ----- | ...
+// [head: 10, next: 20] => [head: 20, next: 55] => [head: 55, next: null] => null
+class Node {
+  next: Node | null = null
+
+  constructor (public data: number) {}
+}
+
+export class LinkedList {
+  head: Node | null = null
+
+  // Add a new Node to the list
+  add(data: number): void {
+    const node = new Node(data)
+
+    if (!this.head) {
+      this.head = node
+      return
+    }
+
+    let tail = this.head
+
+    while (tail.next) {
+      tail = tail.next
+    }
+
+    tail.next = node
+  }
+
+  // Get the length of the list
+  get length(): number {
+    if (!this.head) return 0
+
+    let length = 1
+    let node = this.head
+
+    while (node.next) {
+      length++
+    }
+
+    return length
+  }
+
+  // Get the data of a Node in index N
+  at(index: number): Node {
+    const notFoundMessage = 'Index out of bounds'
+
+    if (!this.head) {
+      throw new Error (notFoundMessage)
+    }
+
+    let counter = 0
+    let node: Node | null = this.head
+
+    while (node) {
+      if (counter === index) {
+        return node
+      }
+      counter++
+      node = node.next
+    }
+
+    throw new Error(notFoundMessage)
+  }
+
+  // Compare two Nodes
+  compare(leftIndex: number, rightIndex: number): boolean {
+    if (!this.head) {
+      throw new Error('List is empty')
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data
+  }
+
+  // Swap head values of two Nodes - Not actually swaping two Nodes
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex)
+    const rightNode = this.at(rightIndex)
+    const leftHand = leftNode.data
+    leftNode.data = rightNode.data
+    rightNode.data = leftHand
+  }
+
+  // Print the Nodes
+  print(): void {
+    if (!this.head) {
+      return
+    }
+    
+    let node: Node | null = this.head
+
+    while (node) {
+      console.log(node.data)
+      node = node.next
+    }
+  }
+}
